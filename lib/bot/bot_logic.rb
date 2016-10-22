4f48d40f9b245a05b77dad9def20c64d69315078
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-10-22 20:43:49 +0200

    Add bot demo logic

--- lib/bot/bot_logic.rb
+++ lib/bot/bot_logic.rb
@@ -5,7 +5,7 @@ class BotLogic < BaseBotLogic
 	def self.setup
 		set_welcome_message "Welcome!"
 		set_get_started_button "bot_start_payload"
-		set_bot_menu
+    set_bot_menu ['Reset', 'Main Menu']
 	end
 
 	def self.cron
@@ -15,40 +15,88 @@ class BotLogic < BaseBotLogic
 	def self.bot_logic
 		ENV["DOMAIN_NAME"] = "https://82be97d0.ngrok.io"
 
-		#binding.pry
+    #binding.pry
 
 		if @request_type == "CALLBACK" and @fb_params.payload == "RESET_BOT"
 			@current_user.delete
 			reply_message "Removed all your data from our servers."
 			return
+		elsif @request_type == "CALLBACK" and @fb_params.payload == "MAIN MENU_BOT"
+      state_go 0
 		end
 
 		state_action 0, :greeting
-		state_action 1, :subscribe
-		state_action 2, :confirm
-		state_action 3, :onboarded
+		state_action 1, :menu_chosen
+		state_action 2, :new_visa_question
+    state_action 3, :when_where_renew_visa
+    state_action 4, :renew_visa_district_chosen
 	end
 
 	def self.greeting
-		reply_message "To make it a little easy. Could you type your due date again just this way: 28-04-2017?"
+    response = HTTParty.get("https://graph.facebook.com/v2.6/#{@fb_params.messaging["sender"]["id"]}?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=EAAJXg6vFWM4BAHGYaZCZC2IGzTqreHyRsaIhYw3Qt6RWBE5FsIjPy7uSMZATfTItjUW3qkqSMW5vtediwRri7y9NvFVQnmez6iZAeTUqZCuF5WpA9hJvyPkEJ6Orbg5dYu2eECuTJFdTW2xTdqS9RYvVhQe0WDDBaPYZCCHV3OYwZDZD")
+    user_info = JSON.parse(response.body)
+		reply_message "Hey #{user_info['first_name']}! How are you liking life in Vienna?"
+    reply_quick_reply "What can I help you with?", ['🇦🇹 Visa', '🏡 Housing', '🎓 University', '🎒 Student life', '🏥 Counseling']
 		state_go
-	end 
+	end
 
-	def self.subscribe
-		due_date = Date.parse get_message
+  def self.menu_chosen
+    case get_message
+    when '🇦🇹 Visa'
+      reply_quick_reply "Is this your first time applying for a visa?", ['✅ Yes', '❌ No']
+      state_go
+    else
+      reply_message "Yeah yeah working on it"
+      state_go 0
+    end
+  end
 
-		@current_user.profile = {due_date: due_date.to_s}
-		@current_user.save!
+  def self.new_visa_question
+    case get_message
+    when '❌ No'
+      reply_quick_reply "Alright! What's on your mind?", ['🕐 When/where to go?', '❓ What do I need"', '⚠️ I was rejected!']
+      state_go
+    else
+      reply_message "Yeah yeah working on it"
+      state_go 0
+    end
+  end
 
-		reply_quick_reply "Okay #{due_date.to_s}. Did I get it right?"
-		state_go		
-	rescue ArgumentError
-		reply_message "{Sorry I do not undestand this format|Can you try again? Format is DD/MM/YYYY}"
-	end 
+  def self.when_where_renew_visa
+    case get_message
+    when '🕐 When/where to go?'
+      reply_quick_reply "That depends. What district do you live in?", %W(1st 2nd 3rd 4th 5th 6th 7th 8th 9th 10th+)
+      state_go
+    else
+      reply_message "Yeah yeah working on it"
+      state_go 0
+    end
+  end
+
+  def self.renew_visa_district_chosen
+    case get_message
+    when '8th'
+      reply_message 'Alright, you need to go renew your visa at the "MA 35 Einwanderung und Staatsbürgerschaft für die Bezirke" near the city hall (Rathaus)'
+      reply_message 'Below is a map of the area:'
+      reply_image "http://i.imgur.com/OfAXVSP.png"
+      reply_message 'Their opening times are as follows:'
+      reply_message 'Monday: 8AM–12PM'
+      reply_message 'Tuesday: 8AM–12PM'
+      reply_message 'Wednesday: Closed'
+      reply_message 'Thursday: 8AM–12PM, 3:30–5:30PM'
+      reply_message 'Friday: 8AM–12PM'
+      reply_message 'Saturday: Closed'
+      reply_message 'Sunday: Closed'
+      reply_quick_reply "Anything else I can do?", ['🕐 When/where to go?', '❓ What do I need"', '⚠️ I was rejected!', '🔙 Go back']
+    else
+      reply_message "Yeah yeah working on it"
+      state_go 0
+    end
+  end
 
 	def self.confirm
 		if get_message == "Yes"
-			subscribe_user("pregnant")	
+			subscribe_user("pregnant")
 			state_go
 			reply_message "Awwww sweet! You are all set now. I'll start to track your pregnancy for you. Can't wait :bride_with_veil::heart::baby_bottle:"
 		else
@@ -56,17 +104,17 @@ class BotLogic < BaseBotLogic
 			@current_user.profile = {}
 			@current_user.save!
 			state_reset
-		end		
+		end
 	end
 
 	def self.onboarded
-		output_current_week		
-	end 
+		output_current_week
+	end
 
 	### helper functions
 
 	def self.calculate_current_week
-		user_date = Date.parse @current_user.profile[:due_date] 
+		user_date = Date.parse @current_user.profile[:due_date]
 		server_date = Date.parse Time.now.to_s
 
 		40 - ((user_date - server_date).to_i / 7)
